9a498d40f27702558a1b8b4ba90adf46d0d6e76e
tarique312 <tariquemdofficial@gmail.com>
2024-06-10 15:25:40 +0530

    added all files of frontend and backend

---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Conversation from "./Conversation";
+import useGetConversations from "../../hooks/useGetConversations";
+import { getRandomEmoji } from "../../utils/emojis";
+
+const Conversations = () => {
+  const { loading, conversations } = useGetConversations();
+  // console.log("CONVERSATION:", conversations);
+  return (
+    <div className="py-2 flex flex-col overflow-auto">
+      {conversations.map((conversation, idx) => (
+        <Conversation
+          key={conversation._id}
+          conversation={conversation}
+          emoji={getRandomEmoji()}
+          lastIdx={idx === conversation.length - 1}
+        />
+      ))}
+      {loading ? <span className="loading loading-spinner"></span> : null}
+    </div>
+  );
+};
+
+export default Conversations;
